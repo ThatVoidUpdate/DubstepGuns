c3604221a1c81621267ca1e18b81b3b1534a8911
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-02-16 06:49:19 +0000

    Changed particles. Changed song. Rewrote spectrum emitter to use frequencies directly

--- Assets/Scripts/Spectrum.cs
+++ Assets/Scripts/Spectrum.cs
@@ -5,41 +5,49 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Spectrum : MonoBehaviour
 {
-
-    public ParticleSystem[] particleSystems;
-    [Range(0.0f,1.0f)]
+    public float[] Frequencies;
+    public float FrequencyTolerance;
+    [Range(0.0f, 1.0f)]
     public float[] Cutoffs;
-    public float[] Scalers;
+    public int[] ParticleAmounts;
+    public ParticleSystem[] Systems;
+
+
+    public int NumSamples = 1024;
+
+    private float MaxFrequency;
+
+    private float[] FreqData;
+
+    public void Start()
+    {
+        MaxFrequency = AudioSettings.outputSampleRate / 2;
+    }
+
     void Update()
     {
-        float[] spectrum = new float[2048];
-        float[] CompressedSpectrum = new float[32];
+        FreqData = new float[NumSamples];
 
-        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
-
-        for (int i = 1; i < spectrum.Length - 1; i++)
+        for (int i = 0; i < Frequencies.Length; i++)
         {
-            //Debug.DrawLine(new Vector3(i - 1, spectrum[i - 1] *100, 0), new Vector3(i, spectrum[i] * 100, 0), Color.red);
-            //Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+            float LowFrequency = Frequencies[i] - FrequencyTolerance;
+            float HighFrequency = Frequencies[i] + FrequencyTolerance;
+
+            AudioListener.GetSpectrumData(FreqData, 0, FFTWindow.BlackmanHarris);
+            int n1 = (int)Mathf.Floor(LowFrequency * NumSamples / MaxFrequency);
+            int n2 = (int)Mathf.Floor(HighFrequency * NumSamples / MaxFrequency);
+            float sum = 0;
+
+            for (var j = n1; j <= n2; j++)
+            {
+                sum += FreqData[j];
+            }
+
+            if (sum > Cutoffs[i])
+            {
+                Systems[i].Emit(ParticleAmounts[i]);
+            }
         }
 
-
-
-        for (int i = 1; i < CompressedSpectrum.Length; i++)
-        {
-            for (int j = i * spectrum.Length / CompressedSpectrum.Length; j < (i + 1) * spectrum.Length / CompressedSpectrum.Length; j++)
-            {
-                CompressedSpectrum[i] += spectrum[j];
-            }
-            //system.Emit((int)(CompressedSpectrum[i] * 10));
-            Debug.DrawLine(new Vector3((i - 1) * (spectrum.Length / CompressedSpectrum.Length), CompressedSpectrum[i] * 100, 0), new Vector3(i * (spectrum.Length / CompressedSpectrum.Length), CompressedSpectrum[i] * 100, 0), Color.yellow);
-            if (particleSystems[i] != null && CompressedSpectrum[i] > Cutoffs[i])
-            {
-                particleSystems[i].Emit((int)(CompressedSpectrum[i] * 100 * Scalers[i]));
-            }
-            
-        }
     }
 }
